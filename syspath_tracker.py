e8c0aebcad36504c186bd049bbc398b61f0ac03f
Jakub Wilk <jwilk@jwilk.net>
2024-03-21 20:27:03 +0100

    Put state variables into namespace.

--- syspath_tracker.py
+++ syspath_tracker.py
@@ -25,12 +25,12 @@ def strjoin(lst):
 def lformat(lst):
     return '[\n{0}]'.format(strjoin('  {0!r},\n'.format(item) for item in lst))
 
-orig_sys_path = None
-tfile = None
+class state:
+    sys_path = None
+    file = None
 
 def trace_sys_path(frame, event, arg):
     del event, arg
-    global orig_sys_path
     try:
         sys_path = sys.path
     except AttributeError:
@@ -39,22 +39,20 @@ def trace_sys_path(frame, event, arg):
     if sys_path is None:
         # could happen during interpreter cleanup
         return
-    if sys_path != orig_sys_path:
-        print('sys.path changed at {f.f_code.co_filename}:{f.f_lineno}:'.format(f=frame), end='', file=tfile)
-        orig_sys_path_f = lformat(orig_sys_path).splitlines()
+    if sys_path != state.sys_path:
+        print('sys.path changed at {f.f_code.co_filename}:{f.f_lineno}:'.format(f=frame), end='', file=state.file)
+        state.sys_path_f = lformat(state.sys_path).splitlines()
         sys_path_f = lformat(sys_path).splitlines()
-        diff = difflib.unified_diff(orig_sys_path_f, sys_path_f, n=9999)
+        diff = difflib.unified_diff(state.sys_path_f, sys_path_f, n=9999)
         for line in itertools.islice(diff, 3, None):
-            print(line, file=tfile)
-        tfile.flush()
-    orig_sys_path = list(sys_path)
+            print(line, file=state.file)
+        state.file.flush()
+    state.sys_path = list(sys_path)
     return trace_sys_path
 
 def install(file=sys.stderr):
-    global tfile
-    global orig_sys_path
-    tfile = file
-    orig_sys_path = list(sys.path)
+    state.file = file
+    state.sys_path = list(sys.path)
     print('Initial sys.path = ' + lformat(sys.path), file=file)
     sys.settrace(trace_sys_path)
 
